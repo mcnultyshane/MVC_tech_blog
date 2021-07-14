bce395843f913664fe6ed9c1ecf0dc31f5ff45b2
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-13 21:17:48 -0400

    added session details to the server.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-// sessions 
+const session = require('express-session')
 
 
 const routes = require('./controllers');
@@ -11,17 +11,29 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 
-// const sess
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-// app.use
+const sess = {
+  secret: '',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
