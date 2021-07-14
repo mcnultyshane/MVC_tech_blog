144a242fc3ac2d73186b091f530a213d449d7d68
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-13 22:18:19 -0400

    working on index in controller routes and api routes.

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+
+
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({  });
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
